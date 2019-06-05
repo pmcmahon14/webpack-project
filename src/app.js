46a272be853de2e9e6875c9a0ed310412fb05e62
Patrick McMahon <pmcmahon14@gmail.com>
2019-06-04 19:06:08 -0700

    setup webpack config

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Link, Route} from 'react-routeer-dom';
+import {Link, Route} from 'react-router-dom';
 
 import Users from './containers/Users';
 import asyncComponent from './hoc/asyncComponent';
@@ -14,7 +14,7 @@ class App extends Component {
         return (
             <div>
                 <div>
-                    <Link to="/">Users</Link>
+                    <Link to="/">Users</Link> |
                     <Link to="/pizza">Pizza</Link>
                 </div>
                 <div>
